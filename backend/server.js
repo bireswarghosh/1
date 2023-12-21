5d18210486079b0293b60501ea1879c39b64754c
bireswarghosh <116499672+bireswarghosh@users.noreply.github.com>
2023-12-21 17:58:48 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -13,6 +13,36 @@ cloudinary.v2.config({
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
+
+// print a working massage on browser screen
+app.get("/", (request, response) => {
+  response.send(`
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>TESTING  Message</title>
+        <style>
+            html, body {
+                height: 100%;
+                margin: 0;
+                display: flex;
+                justify-content: center;
+                align-items: center;
+                font-family: Arial, sans-serif;
+            }
+        </style>
+    </head>
+    <body>
+    
+  
+    <h1>⚒FINE !!  BACKEND IS WORKING ⚒ </h1>
+    </body>
+    </html>
+  `);
+});
+
 app.listen(process.env.PORT, () => {
   console.log(`Server is running on port: ${process.env.PORT} `);
 });
