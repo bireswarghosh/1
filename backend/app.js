c2100c66c850325a566ee5ab0a2bb7b5153416e2
bireswarghosh <116499672+bireswarghosh@users.noreply.github.com>
2023-12-22 00:57:21 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,10 @@
 import express from "express";
 import cookieParser from "cookie-parser";
+import cors  from  "cors";
 // import path from "path";
 export const app = express();
 
+app.use(cors());
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 app.use(cookieParser());
